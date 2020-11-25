6ef9e95ff56c4f0a6ccc1426ca6335a04decbc5a
ishmam ali <ishmamalikhan@gmail.com>
2020-11-25 14:16:24 +0500

    Types in Typescript

---
+++ main.ts
@@ -0,0 +1,34 @@
+// export{};
+
+
+// /// Number Variable
+// let myNum:Number;
+//  myNum =1;
+
+
+///String Variable
+// let myStr:string;
+// myStr="Welcome"
+
+
+////Boolean Variable
+// let myBool:boolean;
+// myBool=true
+
+
+// /// Any Variable
+// let myAnyType : any;
+// myAnyType="Kuch Bhi"
+
+
+/// Array String
+// let arrStr: string[];
+// arrStr=["Hello","World"]
+
+
+/// Array Number
+// let arrNum:number[];
+// arrNum=[1,2,3,4,5]
+
+
+console.log(arrNum);
