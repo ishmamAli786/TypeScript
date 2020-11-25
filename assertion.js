6cfca4e9e7e2931ae06a3dc17414aed9f5e81056
ishmam ali <ishmamalikhan@gmail.com>
2020-11-25 14:28:22 +0500

    Type Assertion

---
+++ assertion.js
@@ -0,0 +1,8 @@
+/// Type Assertion
+/// Type assertion have two forms.
+/// One is the "angle-bracket" syntax:
+/// The Other is as Syntax:
+var name;
+name = "ishmam";
+name.length;
+// (name as string)
---
+++ assertion.ts
@@ -0,0 +1,14 @@
+
+
+/// Type Assertion
+/// Type assertion have two forms.
+/// One is the "angle-bracket" syntax:
+/// The Other is as Syntax:
+
+
+
+let name;
+name = "ishmam";
+(<string>name).length;
+
+// (name as string)
