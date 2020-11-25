1ae006ec13ef3b975c33c580c8f2d2e8db5138d3
ishmam ali <ishmamalikhan@gmail.com>
2020-11-25 13:57:37 +0500

    Variable Declaration

--- types.js
+++ types.js
@@ -1,1 +1,7 @@
-console.log("Welcome To My Channel");
+function subscribe() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log("Finally OutSide The Bloack" + i);
+}
+subscribe();
